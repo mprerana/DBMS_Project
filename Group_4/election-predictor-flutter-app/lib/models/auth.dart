2094836121143878c165fa2390c50b95db0e30a0
Manoj Kumar Manyala <manojmanyala@gmail.com>
2019-05-01 22:25:35 +0530

    fixed conflcits

--- Group_4/election-predictor-flutter-app/lib/models/auth.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-enum AuthMode {
-  Signup,
-  login,
-  Signup_party,
-}
--- Group_4/election-predictor-flutter-app/lib/models/post.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Post {
-
-  String title;
-  String body;
-
-  Post({@required this.title, @required this.body});
-}
